e0b1609cb6f25bf80b4ac520ac9a65a24a153283
raj bora <boraraj682@gmail.com>
2023-04-15 23:35:24 +0530

    sign in page updates on 15/04/2023

--- src/main/java/com/smart/controller/HomeController.java
+++ src/main/java/com/smart/controller/HomeController.java
@@ -7,10 +7,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -71,6 +68,14 @@ public class HomeController {
         User res= this.userRepository.save(user);
         model.addAttribute("user", res);
         return "signup";
+    }
+
+  // custom login page handler
+    @GetMapping("/signin")
+    public  String customLogin( Model m){
+
+       return "login";
+
     }
 
 
